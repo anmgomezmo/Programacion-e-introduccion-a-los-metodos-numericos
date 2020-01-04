4243efd035fc38dd56090ba6dc8b62b380bb9c0f
Slackware Live User <live@darkstar.example.net>
2020-01-03 19:04:35 -0800

    Projecteuler

--- Ejercicios proyecteuler/problem3.cpp
+++ Ejercicios proyecteuler/problem3.cpp
@@ -1,32 +1,22 @@
 #include <iostream>
 #include <cmath>
 
-void print_numbers(int n, unsigned long int m);
-int isprime(unsigned long int n);
+void print_numbers(unsigned long int m);
 
 int main(void){
 
-  print_numbers(1, 600851475143);
+  print_numbers(600851475143);
   
   return 0;
 }
 
-void print_numbers(int n, unsigned long int m){
-  for(int ii = n;ii <= m;++ii){
-    if(isprime(ii)){
-      std::cout << isprime(ii) << "\t" << ii  << std::endl; 
+void print_numbers(unsigned long int m){
+  for(int ii = 2;ii <= m;++ii){
+    if(m%ii == 0){
+      std::cout << ii << std::endl;
+      m = m/ii;
     }
   }
 }
+// aqui me estoy ahorrando un loop extra, no tengo que hacer la division para 2, y para el nuevo m otra vez 2, en vez de eso, para los pares muestra multiplos de 2 que es lo mismo
 
-int isprime(unsigned long int n){
-  if(n == 1) return 0;
-  int target = 1;
-  for(int ii = 2;ii <= std::sqrt(n);++ii){
-    if(n%ii == 0){
-      target = 0;
-      break;
-    }
-  }
-  return target;
-}
---
+++ Ejercicios proyecteuler/problem4.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void){
+
+  int n = 9009;
+
+  for(int ii = 1;ii <= 1000;ii *= 10){
+    std::cout << 9009/ii << std::endl;
+  }
+  
+  return 0;
+}
